00489b286e3825d4888a254033087f14a53d303b
gitHerman0 <hermanbinsatira@gmail.com>
2026-09-21 14:59:05 +0700

    Update HTML titles in app.py

--- app.py
+++ app.py
@@ -39,11 +39,11 @@ st.markdown(
 )
 
 st.markdown(
-    "<h2 class='main-title'>E-RAPOR</h2>",
+    "<h2 class='main-title'>E-RAPORT</h2>",
     unsafe_allow_html=True,
 )
 st.markdown(
-    "<h4 class='sub-title'>KELAS KHUSUS</h4>", unsafe_allow_html=True
+    "<h4 class='sub-title'>JEPANG & DIGIPRENEUR</h4>", unsafe_allow_html=True
 )
 
 # Sumber Data Excel (Membaca otomatis dari file lokal atau upload sidebar)
